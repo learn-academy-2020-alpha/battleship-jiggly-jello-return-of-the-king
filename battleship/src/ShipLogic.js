b62f67598027b64c3b84ed7e08a8e04be1df6036
Xena889988 <brenna889988@gmail.com>
2020-02-24 16:58:41 -0800

    second commit

--- battleship/src/ShipLogic.js
+++ battleship/src/ShipLogic.js
@@ -1,73 +1,47 @@
+
 export default class ShipLogic {
-    constructor(){
-        //console.log("Ships Ahoy!")
 
-        this.maxShips = 5
-        this.maxSize = 5
-        function ShipBlueprint() {
-            this.size = 0
-            this.tiles = []
-            //length 0 means dead
+    // Constants
+    static get maxShips()  { return 5 }
+    static get maxSize()   { return 5 }
+
+    // Ship Object
+    static get ShipBlueprint() {
+        class ShipBlueprint {
+            constructor(){
+                this.size = 0
+                this.tiles = []
+                //length 0 means dead
+            }
         }
-        this.Ship = new ShipBlueprint()
-
-
-        //ShipLogic.generateShips()
-/*
-        var A = new Ship()
-        console.log(A)
-*/
-
-
-
-
-    //var A = new Ship()
-    //A.tiles
-    //A.Size
-
-
-
-
-
-
-
-    //console.log(A)
-
-
-
-
-
+        return ShipBlueprint
     }
 
 
-    generateShips = () => {
-        let ships = new Array(this.maxShips)
-        for(let c=1; c<=this.maxSize; c++){
-            ships[c-1] = Object.create(this.Ship)
-            ships[c-1].size = c
-            ships[c-1].tiles = this.getAvailableTiles(c)
 
-            //console.log(ships[c-1].size)
-            //console.log(ships[c-1].tiles)
+    // Ship Logistics
+    static generateShips() {
+        let ships = new Array(ShipLogic.maxShips)
+        for(let c=1; c<=ShipLogic.maxSize; c++){
+            ships[c-1] = new ShipLogic.ShipBlueprint()
+            ships[c-1].size = c
+            ships[c-1].tiles = ShipLogic.getAvailableTiles(c)
         }
 
         let compareTiles = []
-        for(let c=0; c<this.maxShips; c++){
+        for(let c=0; c<ShipLogic.maxShips; c++){
             compareTiles = compareTiles.concat(ships[c].tiles)
         }
         let compareDuplicates = new Set(compareTiles).size
-        // console.log(compareTiles)
-        //console.log(compareTiles.length - compareDuplicates)
 
         if(compareTiles.length - compareDuplicates > 0){
-            [ships, compareTiles] = this.generateShips()     //  FIXED  IT !!
+            [ships, compareTiles] = ShipLogic.generateShips()     //  FIXED  IT !!
         }
-
         return [ships, compareTiles]
     }
 
 
-    getAvailableTiles = (shipSize) => {
+    static getAvailableTiles( shipSize ) {
         let flipped = Math.floor(Math.random()*2)
         let tilesOut = new Array(shipSize)
 
@@ -91,10 +65,6 @@ export default class ShipLogic {
                 startingLocation+=10
             }
         }
-
-
-        return tilesOut //some number array
+        return tilesOut
     }
-
-
 }
